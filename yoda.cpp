9cca2dcfc9bb55a817f6f1a4495969d241e82261
Nicolas Wicki <nwicki@ethz.ch>
2021-08-26 21:50:02 +0200

    Problem ID: yoda - still failing...

--- yoda.cpp
+++ yoda.cpp
@@ -5,24 +5,33 @@
 #include <iostream>
 #include <cmath>
 #include <vector>
+#include <cassert>
 
 using namespace std;
 
-void convert(int n, vector<int>& vector) {
+const int EXPONENT = 9;
+
+int convert(int n, vector<int>& vector) {
     int i = (int) vector.size() - 1;
-    while(n != 0 || i < 0) {
+    while(n != 0 || i == -1) {
         vector[i] = n % 10;
         n /= 10;
         i--;
     }
+    return (int) vector.size() - i - 1;
 }
 
-void filter(vector<int>& vector1, vector<int>& vector2) {
+int filter(vector<int>& vector1, vector<int>& vector2) {
+    int count = 0;
     for(int i = 0; i < vector1.size(); i++) {
-        if(vector1[i] < vector2[i]) {
-            vector1[i] = 0;
+        int m = vector1[i];
+        int n = vector2[i];
+        if(m < n) {
+            vector1[i] = -1;
+            count++;
         }
     }
+    return count;
 }
 
 int getNumber(const vector<int>& vector) {
@@ -30,7 +39,7 @@ int getNumber(const vector<int>& vector) {
     int sum = 0;
     for(int i = 0; i < vector.size(); i++) {
         int a = vector[vector.size()-1-i];
-        if(a) {
+        if(a != -1) {
             sum += multiplier * a;
             multiplier *= 10;
         }
@@ -38,8 +47,8 @@ int getNumber(const vector<int>& vector) {
     return sum;
 }
 
-void output(int result) {
-    if(result == 0) {
+void output(bool yoda, int result) {
+    if(yoda) {
         cout << "YODA" << endl;
     }
     else {
@@ -47,19 +56,26 @@ void output(int result) {
     }
 }
 
+void compute(int n, int m) {
+    assert(n <= pow(10,EXPONENT));
+    assert(m <= pow(10,EXPONENT));
+    vector<int> vector_n(EXPONENT,-1);
+    vector<int> vector_m(EXPONENT,-1);
+    int length_n = convert(n,vector_n);
+    int length_m = convert(m,vector_m);
+    int score_n = filter(vector_m,vector_n);
+    int score_m = filter(vector_n,vector_m);
+    output(score_m == length_n, getNumber(vector_n));
+    output(score_n == length_m, getNumber(vector_m));
+}
+
 int main() {
-    int n,m,exponent = 9;
+    int n,m;
     cin >> n;
     cin >> m;
-    assert(n <= pow(10,9));
-    assert(m <= pow(10,9));
-    vector<int> vector_n(exponent,0);
-    vector<int> vector_m(exponent,0);
-    convert(n,vector_n);
-    convert(m,vector_m);
-    filter(vector_n,vector_m);
-    filter(vector_m,vector_n);
-    output(getNumber(vector_n));
-    output(getNumber(vector_m));
+    compute(n,m);
+//    compute(300,500);
+//    compute(65743,9651);
+//    compute(2341,6785);
     return 0;
 }
