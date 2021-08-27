bc43fe20382ae9b7d9c898033477a5ae2a2769f5
Nicolas Wicki <nwicki@ethz.ch>
2021-08-27 10:13:46 +0200

    Problem ID: yoda, Difficulty: 2.0 - DONE

--- yoda.cpp
+++ yoda.cpp
@@ -3,70 +3,60 @@
 //
 
 #include <iostream>
-#include <cmath>
 #include <vector>
-#include <cassert>
 
 using namespace std;
 
-const int EXPONENT = 9;
-
-int convert(int n, vector<int>& vector) {
-    int i = (int) vector.size() - 1;
-    while(n != 0 || i == -1) {
-        vector[i] = n % 10;
+void getVector(int n, vector<int>& vector) {
+    while(n != 0) {
+        vector.push_back(n % 10);
         n /= 10;
-        i--;
     }
-    return (int) vector.size() - i - 1;
 }
 
-int filter(vector<int>& vector1, vector<int>& vector2) {
-    int count = 0;
-    for(int i = 0; i < vector1.size(); i++) {
-        int m = vector1[i];
-        int n = vector2[i];
-        if(m < n) {
-            vector1[i] = -1;
-            count++;
+void collide(const vector<int>& vectorATK, const vector<int>& vectorDEF, vector<int>& vectorSurvived) {
+    auto itATK = vectorATK.begin();
+    auto itDEF = vectorDEF.begin();
+    while(itATK != vectorATK.end() && itDEF != vectorDEF.end()) {
+        int def = *itDEF;
+        if(*itATK <= def) {
+            vectorSurvived.push_back(def);
         }
+        itATK++;
+        itDEF++;
     }
-    return count;
+    while(itDEF != vectorDEF.end()) {
+        vectorSurvived.push_back(*itDEF);
+        itDEF++;
+    }
 }
 
-int getNumber(const vector<int>& vector) {
-    int multiplier = 1;
-    int sum = 0;
-    for(int i = 0; i < vector.size(); i++) {
-        int a = vector[vector.size()-1-i];
-        if(a != -1) {
-            sum += multiplier * a;
-            multiplier *= 10;
-        }
-    }
-    return sum;
-}
-
-void output(bool yoda, int result) {
-    if(yoda) {
+void output(const vector<int>& vector) {
+    if(vector.size() == 0) {
         cout << "YODA" << endl;
     }
     else {
-        cout << result << endl;
+        int sum = 0;
+        int multiplier = 1;
+        for(int i = 0; i < vector.size(); i++) {
+            sum += multiplier * vector[i];
+            multiplier *= 10;
+        }
+        cout << sum << endl;
     }
 }
 
 void compute(int n, int m) {
-    assert(n <= pow(10,EXPONENT));
-    assert(m <= pow(10,EXPONENT));
-    vector<int> vector_n(EXPONENT,-1);
-    vector<int> vector_m(EXPONENT,-1);
-    int length_n = convert(n,vector_n);
-    int length_m = convert(m,vector_m);
-    int score_n = filter(vector_m,vector_n);
-    int score_m = filter(vector_n,vector_m);
-    output(score_m == length_n, getNumber(vector_n));
-    output(score_n == length_m, getNumber(vector_m));
+    vector<int> vector_n;
+    vector<int> vector_m;
+    vector<int> vector_nx;
+    vector<int> vector_mx;
+    getVector(n,vector_n);
+    getVector(m,vector_m);
+    collide(vector_m,vector_n,vector_nx);
+    collide(vector_n,vector_m,vector_mx);
+    output(vector_nx);
+    output(vector_mx);
 }
 
 int main() {
